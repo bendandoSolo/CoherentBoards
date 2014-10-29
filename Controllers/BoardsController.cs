55ea7f1cc96cbbe582b01ff64a4bbf4dc5cffdc7
bendando <ben.roberts@bendando.com>
2014-10-29 14:11:01 +0000

    Currently Working Version

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoherentBoards.Models;
+
+namespace CoherentBoards.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private BoardEntities db = new BoardEntities();
+
+        // GET: Boards
+        public ActionResult Index()
+        {
+            return View(db.Boards.ToList());
+        }
+        
+        // GET: Boards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Boards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Version,Target,Variant,Flags")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Boards.Add(board);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(board);
+        }
+
+        // GET: Boards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Boards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Version,Target,Variant,Flags")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(board).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(board);
+        }
+
+        // GET: Boards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Boards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Board board = db.Boards.Find(id);
+            db.Boards.Remove(board);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
